67d6ac3ab6a155dd2e18ac3a9e60c6651dc5716b
HDwayne <hdwayne2002@gmail.com>
2024-12-14 20:23:36 +0100

    test need bigger range

--- app_analyse/test.sh
+++ app_analyse/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Iterate over the ranges
-for h in $(seq 4500 500 7500); do
-    for w in $(seq 4500 500 7500); do
+for h in $(seq 4500 500 10000); do
+    for w in $(seq 4500 500 10000); do
         echo "Running command with -h $h -w $w"
         sudo ./app --json ../fractal/fractal -h "$h" -w "$w" -f
     done
